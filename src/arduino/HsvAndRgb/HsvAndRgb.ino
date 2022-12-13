025793db931c94e51a8107072db74bede6f01ce0
Matthew Whited <matt@whited.us>
2022-12-13 13:09:50 -0500

    change back to 10 bit and fix variables

--- src/arduino/HsvAndRgb/HsvAndRgb.ino
+++ src/arduino/HsvAndRgb/HsvAndRgb.ino
@@ -3,7 +3,7 @@ int red_led = 9;
 int green_led = 10;
 int blue_led = 11;
 
-int isHsv_switch = 7;
+int isRGB_switch = 7;
 
 int delay_time = 1;
 int step = 5;
@@ -21,21 +21,27 @@ struct ColorRgb {
   uint8_t blue;
 };
 
-double potMax = 255.0;
+double potMax = 0x3fa;
+int downsample = 0;
+int downsampleRGB = 2;
 
 #define abs(x) ((x)>0?(x):-(x))
 
-ColorRgb fromHsv(uint8_t h, uint8_t s, uint8_t v){
+ColorRgb fromHsv(int h, int s, int v){
 
-  double hDegree = (h / potMax) * 359.9;
+  if (h > potMax) h = potMax; 
+  if (s > potMax) s = potMax; 
+  if (v > potMax) v = potMax; 
+
+  double hDegree = (h / potMax) * 359.99;
   double sPercent = s / potMax;
   double vPercent = v / potMax;
 
   double c = vPercent * sPercent;
   double hPrime = hDegree / 60.0;
 
-  int hMod = hPrime;
-  hMod = hMod % 2;
+  double hMod = hPrime;
+  while(hMod > 2.0) hMod /= 2.0;
 
   double x = c * (1 - abs(hMod - 1.0));
   double m = vPercent - c;
@@ -43,33 +49,38 @@ ColorRgb fromHsv(uint8_t h, uint8_t s, uint8_t v){
   ColorRgb result;
 
   if (debug){
-    Serial.print("  hDegree= ");
+    Serial.print(" hDegree=");
     Serial.print(hDegree);
-    Serial.print("  sPercent= ");
+    Serial.print("  sPercent=");
     Serial.print(sPercent);
-    Serial.print("  vPercent= ");
+    Serial.print("  vPercent=");
     Serial.print(vPercent);
     
-    Serial.print(" c= ");
+    Serial.print(" c=");
     Serial.print(c);
-    Serial.print(" hPrime= ");
+    Serial.print(" hPrime=");
     Serial.print(hPrime);
-    Serial.print(" x= ");
+    Serial.print(" hMod=");
+    Serial.print(hMod);
+    Serial.print(" x=");
     Serial.print(x);
-    Serial.print(" m= ");
+    Serial.print(" m=");
     Serial.print(m);
   }
 
-       if (0.0 <= hPrime && hPrime < 1.0) result = {(byte)(c *255),(byte)(x *255),(byte)(0 *255)};
-  else if (1.0 <= hPrime && hPrime < 2.0) result = {(byte)(x *255),(byte)(c *255),(byte)(0 *255)};
-  
-  else if (2.0 <= hPrime && hPrime < 3.0) result = {(byte)(0 *255),(byte)(c *255),(byte)(x *255)};
-  else if (3.0 <= hPrime && hPrime < 4.0) result = {(byte)(0 *255),(byte)(x *255),(byte)(c *255)};
-    
-  else if (4.0 <= hPrime && hPrime < 5.0) result = {(byte)(x *255),(byte)(0 *255),(byte)(c *255)};
-  else if (5.0 <= hPrime && hPrime < 6.0) result = {(byte)(c *255),(byte)(0 *255),(byte)(x *255)};
+  double cm = c+m;
+  double xm = x+m;
 
-  else result = {0,0,0};
+       if (0.0 <= hPrime && hPrime < 1.0) result = {(byte)(cm *255),(byte)(xm *255),(byte)(m *255)};
+  else if (1.0 <= hPrime && hPrime < 2.0) result = {(byte)(xm *255),(byte)(cm *255),(byte)(m *255)};
+  
+  else if (2.0 <= hPrime && hPrime < 3.0) result = {(byte)(m *255),(byte)(cm *255),(byte)(xm *255)};
+  else if (3.0 <= hPrime && hPrime < 4.0) result = {(byte)(m *255),(byte)(xm *255),(byte)(cm *255)};
+    
+  else if (4.0 <= hPrime && hPrime < 5.0) result = {(byte)(xm *255),(byte)(m *255),(byte)(cm *255)};
+  else if (5.0 <= hPrime && hPrime < 6.0) result = {(byte)(cm *255),(byte)(m *255),(byte)(xm *255)};
+
+  else result = {m,m,m};
 
   return result;
 }
@@ -82,7 +93,7 @@ void setup() {
   pinMode(green_led, OUTPUT);
   pinMode(blue_led, OUTPUT);
   
-  pinMode(isHsv_switch, INPUT);
+  pinMode(isRGB_switch, INPUT);
 
   if (debug){
     Serial.begin(9600);
@@ -93,26 +104,46 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
 
-  int isHsv = digitalRead(isHsv_switch);
+  int isRGB = digitalRead(isRGB_switch);
 
   int h = analogRead(h_pot);
   int s = analogRead(s_pot);
   int v = analogRead(v_pot);
+  
+  if (debug){
+    Serial.print(" isRGB ");
+    Serial.print(isRGB, HEX);
 
-  if (deadZone == 0) {
-    h = h >>2;
-    s = s >>2;
-    v = v >>2;
+    Serial.print(" AIN ");
+    Serial.print(h, HEX);
+    Serial.print(" ");
+    Serial.print(s, HEX);
+    Serial.print(" ");
+    Serial.print(v, HEX);  
   }
-  else {
-    if (h < deadZone) h = 0;
-    if (s < deadZone) s = 0;
-    if (v < deadZone) v = 0;
+
+
+  if (isRGB){
+    if (deadZone == 0) {
+      h = h >> downsample;
+      s = s >> downsample;
+      v = v >> downsample;
+    }
+    else {
+      if (h < deadZone) h = 0;
+      if (s < deadZone) s = 0;
+      if (v < deadZone) v = 0;
+    }
+  } else {
+    Serial.print(" XXX ");
+    h = h >> downsampleRGB;
+    s = s >> downsampleRGB;
+    v = v >> downsampleRGB;
   }
 
   ColorRgb rgb;
   
-  if (isHsv)
+  if (!isRGB)
     rgb = {h,s,v};
   else 
     rgb = fromHsv(h,s,v);
