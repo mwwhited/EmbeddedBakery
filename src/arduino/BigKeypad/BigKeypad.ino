5b355b3e3d77254f05c9a01828bf818db1d16c20
Matthew Whited <matt.whited@lightwellinc.com>
2023-04-26 17:01:52 -0400

    update keyboard

--- src/arduino/BigKeypad/BigKeypad.ino
+++ src/arduino/BigKeypad/BigKeypad.ino
@@ -1,28 +1,55 @@
+#include <LiquidCrystal_I2C.h>
+#include <Keyboard.h>
+
 /*
 see readme for pin outs
 */
 
-int cols[] = { 12,11,10,9 };
-int rows[] = { 7,6,5,4, 3,2,A5,A4, A3,A2,A1,A0 };
+int cols[] = { A3, A2, A1, A0 };
+int rows[] = { 0, 1, 4, 5, 6, 7, 8, 9, 15, 14, 16, 10 };
 
 char keyMap[] = ">/\\~_KI8<LO9v^P0MJU7VFR4BGT5NHY6++++ZQA1XWS2CED3";
 
-//#define DEBUG 1
+LiquidCrystal_I2C lcd(0x27, 16, 4);  // I2C address 0x27, 16 column and 2 rows
+
+// #define DEBUG 1
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  //Keyboard.begin();
 
-  for(int i = 0; i < 4; i++){
+  while (!Serial) {
+  }
+  Serial.println("Ready!");
+
+  // https://arduinogetstarted.com/tutorials/arduino-lcd-i2c
+  lcd.init();       //initialize the lcd
+  lcd.backlight();  //open the backlight
+  lcd.setCursor(0, 0);
+  lcd.print("Ready!");
+
+  // lcd.setCursor(0, 0);
+  // for (int i = 0; i < 4; i++) {
+  //   for (int j = 0; j < 20; j++) {
+  //     if (j % 2) {
+  //       lcd.print(i % 3 ? "+" : "-");
+  //     } else {
+  //       lcd.print(i % 5 ? "*" : "x");
+  //     }
+  //   }
+  // }
+
+  for (int i = 0; i < 4; i++) {
     pinMode(cols[i], OUTPUT);
   }
 
-  for(int i = 0; i < 12; i++){
+  for (int i = 0; i < 12; i++) {
     pinMode(rows[i], INPUT_PULLUP);
   }
 }
 
-long last[]={0xFFF,0xFFF,0xFFF,0xFFF};
+long last[] = { 0xFFF, 0xFFF, 0xFFF, 0xFFF };
 long colIndex = 0;
 
 void loop() {
@@ -32,7 +59,7 @@ void loop() {
   long read = readRow();
   if (read != last[colIndex]) {
     last[colIndex] = read;
-    int ret  = simpler(colIndex, read);
+    int ret = simpler(colIndex, read);
 
 #ifdef DEBUG
     Serial.print("b");
@@ -49,14 +76,17 @@ void loop() {
     Serial.println();
 #endif
 
-    if (ret) { 
-      if (keyMap[ret & 0x7f] == '\\'){
+    if (ret) {
+      if (keyMap[ret & 0x7f] == '\\') {
+        lcd.setCursor(0, 0);
         Serial.println();
       } else {
-        Serial.print(keyMap[ret & 0x7f]);
+        char key = keyMap[ret & 0x7f];
+        Serial.print(key);
+        lcd.print(key);        
+        // Keyboard.press(key);
       }
     }
-  
   }
 }
 
@@ -67,14 +97,14 @@ MM  -> Keypad
 CCC -> Column
 RRR -> Row
 */
-int simpler(int colVal, long rowVal){
-  for(int i = 0; i < 3; i++){
+int simpler(int colVal, long rowVal) {
+  for (int i = 0; i < 3; i++) {
     int n = nibble(rowVal >> (4 * i));
-    if (n){
+    if (n) {
       long ret = 0b10000000;
       ret |= (n & 0b0011);
       ret |= (colVal << 2);
-      ret |= ((long)i)<<4;
+      ret |= ((long)i) << 4;
       return ret;
     }
   }
@@ -89,14 +119,14 @@ MM  -> Keypad
 CCC -> Column
 RRR -> Row
 */
-int simple(int colVal, long rowVal){
-  for(int i = 0; i < 3; i++){
+int simple(int colVal, long rowVal) {
+  for (int i = 0; i < 3; i++) {
     int n = nibble(rowVal >> (4 * i));
-    if (n){
+    if (n) {
       long ret = 0;
       ret |= (n);
       ret |= (colVal << 3);
-      ret |= ((long)i)<<6;
+      ret |= ((long)i) << 6;
       return ret;
     }
   }
@@ -105,25 +135,25 @@ int simple(int colVal, long rowVal){
 }
 
 int nibble(int val) {
-  switch (val & 0x0f){
+  switch (val & 0x0f) {
     case 0b1110: return 0b0100;
     case 0b1101: return 0b0101;
     case 0b1011: return 0b0110;
     case 0b0111: return 0b0111;
-    default:     return 0b0000;
+    default: return 0b0000;
   }
 }
 
-int setColumn(int colVal){  
-  for(int i = 0; i < 4; i++){
+int setColumn(int colVal) {
+  for (int i = 0; i < 4; i++) {
     digitalWrite(cols[i], colVal == i ? 0 : 1);
   }
   return (colVal + 1) % 4;
 }
 
-long readRow(){  
+long readRow() {
   long outValue = 0;
-  for(int i = 0; i < 12; i++){
+  for (int i = 0; i < 12; i++) {
     outValue |= (digitalRead(rows[i]) << i);
   }
   return outValue;
